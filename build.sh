67ce9e1d6e9978cb12a30203d7695207b76d5173
Li-Yu Yu <afg984@gmail.com>
2022-01-08 18:54:58 +0800

    Remove unused build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-cmake-3.22.1-linux-x86_64/bin/cmake -B build \
-    -S llvm-13.0.0.src \
-    -DCMAKE_TOOLCHAIN_FILE=$PWD/cmake.toolchain \
-    -G Ninja \
-    -DCMAKE_MAKE_PROGRAM=$PWD/ninja \
-    -DLLVM_ENABLE_PROJECTS= \
-    -DLLVM_TABLEGEN=$PWD/clang+llvm-13.0.0-x86_64-linux-gnu-ubuntu-20.04/bin/llvm-tblgen \
-    -DLLVM_TARGETS_TO_BUILD=X86 \
-    -DCMAKE_BUILD_TYPE=Release
